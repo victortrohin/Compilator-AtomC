046c331a409dc98c7f6cb87e421edc16cc1d9929
Victor Trohin <trohin2000@gmail.com>
2022-04-10 14:05:50 +0200

    analizor lexical functional

---
+++ testAlex.c
@@ -0,0 +1,13 @@
+// program de testare a analizorului lexical, v1.1
+
+int main()
+{
+	int i;
+	for(i=0;i<10;i=i+1){
+		if(i/2==1)puti(i);
+		}
+	if(4.9==49e-1&&0.49E1==2.45*2.0)puts("yes");
+	putc('#');
+	puts("");	// pentru \n
+	return 0;
+}
